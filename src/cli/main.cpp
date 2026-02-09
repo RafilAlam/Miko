04d5b71699cf96fdbe83c0ee0fb05831226dad98
Rafil Alam <rafilalam009@gmail.com>
2026-02-09 15:04:55 +0800

    Implemented profile and model loading

--- src/cli/main.cpp
+++ src/cli/main.cpp
@@ -9,11 +9,12 @@
 #include <functional>
 #include <filesystem>
 
-std::map<std::string, std::function<void()>> functions;
+std::map<std::string, std::function<void(std::vector<std::string>)>> functions;
+
+const std::string configDir = std::getenv("HOME") + std::string("/.config/miko");
+const std::string modelDir = std::getenv("HOME") + std::string("/.local/share/miko/");
 
 void loadProfile(std::string& modelpath, std::string& systemprompt) {
-  std::string configDir = std::getenv("HOME") + std::string("/.config/miko");
-  std::string modelDir = std::getenv("HOME") + std::string("/.local/share/miko/");
   if (!std::filesystem::exists(configDir)) {
     std::filesystem::create_directory(configDir);
     std::filesystem::create_directory(configDir + "/profiles");
@@ -23,13 +24,18 @@ void loadProfile(std::string& modelpath, std::string& systemprompt) {
     std::cerr << "Model folder is empty! Please download a gguf and place it in '" << modelDir << "'\n";
     std::exit(1);
   }
-  if (!std::filesystem::exists(configDir + "/miko.conf")) {
-    std::ofstream file(configDir + "/miko.conf");
-    file << "profile_choice=\nmodel_choice=\n";
+  if (!std::filesystem::exists(configDir + "/models.conf")) {
+    std::ofstream file(configDir + "/models.conf");
+    file << "model_choice=\n";
+    file.close();
+  }
+  if (!std::filesystem::exists(configDir + "/profiles.conf")) {
+    std::ofstream file(configDir + "/profiles.conf");
+    file << "profile_choice=\n";
     file.close();
   }
 
-  configmap map = file_to_map(configDir + "/miko.conf");
+  configmap map = file_to_map(configDir + "/models.conf");
   modelpath = map[map["model_choice"]];
   std::cout << map["model_choice"] << std::endl;
   if (modelpath.empty()) {
@@ -37,11 +43,21 @@ void loadProfile(std::string& modelpath, std::string& systemprompt) {
     std::exit(1);
   }
   modelpath = modelDir + modelpath;
+
+  map = file_to_map(configDir + "/profiles.conf");
+  std::string data;
+  std::ifstream file(map[map["profile_choice"]]);
+  while (std::getline(file, data)) {
+    systemprompt += data;
+  }
+
+  if (systemprompt.empty()) {
+    debugLog("WARNING: Running model without a system prompt!");
+  }
 }
 
 void init() {
-  functions["wake"] = []() {
-    std::string homeDir = std::getenv("HOME");
+  functions["wake"] = [](std::vector<std::string> args) {
     std::string modelPath;
     std::string systemPrompt;
 
@@ -59,13 +75,35 @@ void init() {
     }   
   };
 
-  functions["add"] = []() {
+  functions["add"] = [](std::vector<std::string> args) {
     std::cout << "Invalid usage of add.\n add -m <file_name.gguf> - Adds a model to registry.\n add -p <file_name> - Adds a profile to registry.\n";
+    std::exit(1);
   };
 
-  functions["add-m"] = []() {
-    std::cout << "add-m!\n";
+  functions["add-m"] = [](std::vector<std::string> args) {
+    if (args.size()<=3) {
+      functions["add"](args);
+    }
+    else if (!std::filesystem::exists(modelDir)) {
+      std::filesystem::create_directory(modelDir);
+      std::cerr << "Model folder is empty! Please download a gguf and place it in '" << modelDir << "'\n";
+      std::exit(1);
+    }
+    else if (!std::filesystem::exists(configDir + "/models.conf")) {
+      std::ofstream file(configDir + "/models.conf");
+      file << "model_choice=\n";
+      file.close();
+    }
+    configmap map = file_to_map(configDir + "/models.conf");
+    map[args[3]] = args[4];
+    map_to_file(map, configDir + "/models.conf");
   };
+
+  functions["ls"] = [](std::vector<std::string> args) {
+    std::cout << "Invalid usage of ls.\n ls -m - Shows list of models\n ls -p - Shows list of profiles\n";
+  };
+
+  
 }
 
 int main(int argc, const char* argv[]) {
@@ -75,5 +113,5 @@ int main(int argc, const char* argv[]) {
   if (argc>2) {
     function += args[2];
   }
-  functions[function]();
+  functions[function](args);
 }
